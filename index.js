1459068a217274387ea9471f0a4f635772a6f3d2
Abhishek <abhinav210702@gmail.com>
2022-01-01 18:07:11 +0530

    Changes Made for Production

--- index.js
+++ index.js
@@ -27,11 +27,18 @@ mongoose.connect(
 //Middleware
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000", credentials:true }))
+app.set("trust proxy", 1);
+
 app.use(
     session({
         secret: "secretcode",
         resave:true,
         saveUninitialized:true,
+        cookie:{
+          sameSite: "none",
+          secure:true,
+          maxAge:1000 * 60 * 60 * 24 * 7  //one week
+        }
     })
 );
 app.use(cookieParser());
